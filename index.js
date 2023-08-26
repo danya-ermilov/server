ed4115ebf53a0383ee33830f32cde421ef737eb9
danya-ermilov <141646057+danya-ermilov@users.noreply.github.com>
2023-08-26 20:04:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+import config from "dotenv/config";
+import express from "express";
+import sequelize from "./sequelize.js";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import router from "./routes/index.js";
+import ErrorHandler from "./middleware/ErrorHandler.js";
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+// Cross-Origin Resource Sharing
+app.use(cors());
+// middleware для работы с json
+app.use(express.json());
+// middleware для статики (img, css)n
+app.use(express.static("static"));
+// middleware для загрузки файлов
+app.use(fileUpload());
+// все маршруты приложения
+app.use("/api", router);
+
+// обработка ошибок
+app.use(ErrorHandler);
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log("Сервер запущен на порту", PORT));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
+/*
 import fs from "fs";
 import https from "https"; // Импорт модуля https
 
@@ -41,3 +78,4 @@ const start = async () => {
 };
 
 start();
+*/
